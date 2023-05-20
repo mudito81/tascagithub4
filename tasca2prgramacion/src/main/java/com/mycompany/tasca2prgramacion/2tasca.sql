accdba51276d08b5770387c9c0745aee3d3c740b
mudito81 <70726895+mudito81@users.noreply.github.com>
2023-05-20 22:00:28 +0200

    Update 2tasca.sql

--- tasca2prgramacion/src/main/java/com/mycompany/tasca2prgramacion/2tasca.sql
+++ tasca2prgramacion/src/main/java/com/mycompany/tasca2prgramacion/2tasca.sql
@@ -1,4 +1,4 @@
-public class MultiplicationTables {
+public class tasca4 {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println("Taula de multiplicar de " + i + ":");
